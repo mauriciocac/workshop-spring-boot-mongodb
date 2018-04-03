ec64e7782466149219fd7fe9d96d3706b8de6361
mauriciocac <mauricio.cac@hotmail.com>
2018-04-03 13:32:46 -0300

    Usando padrão DTO para retornar os usuários

---
+++ src/main/java/com/oak/wshop/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package com.oak.wshop.dto;
+
+import java.io.Serializable;
+
+import com.oak.wshop.model.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String nome;
+	private String email;
+
+	public UserDTO() {
+	}
+
+	public UserDTO(User user) {
+		id = user.getId();
+		nome = user.getNome();
+		email = user.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- src/main/java/com/oak/wshop/resource/UserResource.java
+++ src/main/java/com/oak/wshop/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.oak.wshop.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oak.wshop.dto.UserDTO;
 import com.oak.wshop.model.User;
 import com.oak.wshop.service.UserService;
 
@@ -23,5 +25,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
+//	@GetMapping
+//	public ResponseEntity<List<UserDTO>> listAll() {
+//		List<User> list = service.findAll();
+//		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+//		return ResponseEntity.ok().body(listDTO);
+//	}
+
 }

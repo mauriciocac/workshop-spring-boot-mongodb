1c7bf2c635268fff9e3b6db4876829a39463a354
mauriciocac <mauricio.cac@hotmail.com>
2018-04-03 09:52:43 -0300

    Primeiro commit do projeto Spring-Mongo

---
+++ src/main/java/com/oak/wshop/WorkshopSpringMongoApplication.java
@@ -0,0 +1,12 @@
+package com.oak.wshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oak/wshop/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oak.wshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopSpringMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
